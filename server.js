1ac0e6283a623a7c355c2dda5a094a6f258b6f93
abhilashalk <abhilulk@gmail.com>
2018-06-08 11:17:18 -0400

    first server js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = 5000;
